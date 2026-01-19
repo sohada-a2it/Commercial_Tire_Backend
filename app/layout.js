8f574dfe71fc1bd8cb952c350fc9d9a0ef9dd876
naima-uddin <toktikproject@gmail.com>
2026-01-19 09:33:01 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -124,6 +124,19 @@ export default function RootLayout({ children }) {
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
         />
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ta1fgz6ov8");
+            `,
+          }}
+        />
       </head>
       <body>
         <CartProvider>
